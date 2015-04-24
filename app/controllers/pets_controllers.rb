0c5efbcf9cdad024161506b936d0c64f9a266980
cbestiva <cbestiva@gmail.com>
2015-04-24 14:04:28 -0700

    added controller methods

--- app/controllers/pets_controllers.rb
+++ app/controllers/pets_controllers.rb
@@ -1,19 +1,36 @@
 class PetsController < ApplicationController
   def index
+    Pet.all
   end
 
   def new
+    @pet = Pet.new(pet_params)
   end
   
   def create
+    @pet = Pet.create(pet_params)
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
   end
 
   def edit
+    @pet = Pet.find(params[:id])
   end
 
   def update
+    @pet = Pet.find(params[:id])
+    @pet.update_attributes(params[:pet].permit(:name, :age, :type)
   end
 
-  def delete
+  def destroy
+    @pet = Pet.find(params[:id]
+    @pet.delete
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :age, :type)
   end
 end
