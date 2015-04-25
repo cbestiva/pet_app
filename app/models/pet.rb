9c75c64ffc21deee4b7871097de71acb1c4c4128
cbestiva <cbestiva@gmail.com>
2015-04-24 17:40:23 -0700

    added capitalization methods

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,12 @@
 class Pet < ActiveRecord::Base
   validates_presence_of :name
+  before_save :capitalize_name, :capitalize_type
+
+  def capitalize_name
+    self.name = self.name.capitalize
+  end
+
+  def capitalize_type
+    self.pet_type = self.pet_type.capitalize
+  end
 end
