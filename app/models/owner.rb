f25d68555a48f54d9a520e801070e91e1df99249
cbestiva <cbestiva@gmail.com>
2015-04-24 19:40:39 -0700

    added capitalization methods to first and last names before saving owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,12 @@
 class Owner < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
+  before_save :capitalize_first_name, :capitalize_last_name
+
+  def capitalize_first_name
+    self.first_name = self.first_name.capitalize
+  end
+
+  def capitalize_last_name
+    self.last_name = self.last_name.capitalize
+  end
 end
