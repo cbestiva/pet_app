2de8aa51781f6e9adc6330b0bfc22aa2fb76d734
cbestiva <cbestiva@gmail.com>
2015-04-27 09:57:05 -0700

    added navigation to index pages, able to edit and delete ownerships

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,6 +1,6 @@
 class OwnershipsController < ApplicationController
   def index
-    @ownerships = Ownership.all
+    @ownerships = Ownership.all.order(pet_id: :asc)
   end
 
   def new
@@ -18,6 +18,30 @@ class OwnershipsController < ApplicationController
       @owners = Owner.all
       @pets = Pet.all
       render :new
+    end
+  end
+
+  def edit
+    @ownership = Ownership.find(params[:id])
+    @owners = Owner.all
+    @pets = Pet.all
+  end
+
+  def update
+    @ownership = Ownership.find(params[:id])
+    @ownership.update_attributes(params[:ownership].permit(:owner_id, :pet_id))
+
+    respond_to do |f|
+      f.html {redirect_to root_url}
+    end
+  end
+
+  def destroy
+    @ownership = Ownership.find(params[:id])
+    @ownership.delete
+
+    respond_to do |f|
+      f.html {redirect_to pets_url}
     end
   end
 
