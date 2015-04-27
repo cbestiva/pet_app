81db8e3a0fef098e5c4778d114ede4f15b76be06
cbestiva <cbestiva@gmail.com>
2015-04-27 11:05:46 -0700

    added methods to capitalize toy name and color before saving to db

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,3 +1,12 @@
 class Toy < ActiveRecord::Base
+  before_save :capitalize_name, :capitalize_color
   belongs_to :pet
+
+  def capitalize_name
+    self.name = self.name.capitalize
+  end
+  
+  def capitalize_color
+    self.color = self.color.capitalize
+  end
 end
